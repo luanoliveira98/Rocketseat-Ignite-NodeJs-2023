1b29613d8f62155661630f5c5577c44f59610cf3
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-13 12:18:19 -0300

    feat: add delete task

--- 01-challenge/src/routes.js
+++ 01-challenge/src/routes.js
@@ -40,7 +40,18 @@ export const routes = [
       const { id } = req.params
       const { title, description } = req.body
 
-      const tasks = database.update('tasks', id, { title, description })
+      database.update('tasks', id, { title, description })
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('tasks', id)
 
       return res.writeHead(204).end()
     }
